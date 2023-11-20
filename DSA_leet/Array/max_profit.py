a14daadafea66a26fecc80fc69c9133c1c084cb9
vaishnave-s <58141948+vaishnave-s@users.noreply.github.com>
2023-11-20 15:01:20 +0000

    leetcode - max profit

---
+++ DSA_leet/Array/max_profit.py
@@ -0,0 +1,10 @@
+class Solution(object):
+	def maxProfit(self, prices):
+			min_price=float("inf")
+			max_profit=0
+			for i in range(len(prices)):
+					if(prices[i]<min_price):
+							min_price=prices[i]
+					elif(prices[i]-min_price>max_profit):
+							max_profit=prices[i]-min_price
+			return max_profit
