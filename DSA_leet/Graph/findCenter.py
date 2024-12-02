1f500465ceae8647717aa97eb129fac9e5bf173f
vaishnave-s <58141948+vaishnave-s@users.noreply.github.com>
2024-12-02 12:47:13 +0530

    Find Center of Star Graph | On, On

--- DSA_leet/Graph/findCenter.py
+++ DSA_leet/Graph/findCenter.py
@@ -1,15 +1,13 @@
 class Solution:
     def findCenter(self, edges: List[List[int]]) -> int:
-        hashmap = {}
+        hashmap =set()
         for n1,n2 in edges:
             if n1 not in hashmap:
-                hashmap[n1]=1
+                hashmap.add(n1)
             else:
-                hashmap[n1]+=1
+                return n1
             if n2 not in hashmap:
-                hashmap[n2]=1
+                hashmap.add(n2)
             else:
-                hashmap[n2]+=1
-        for k,v in hashmap.items():
-            if v!=1:
-                return k
+                return n2
+                
